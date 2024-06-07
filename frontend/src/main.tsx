68e0376ab3afc1e425a36443474672d364c4e966
Price Tula <pricetula@gmail.com>
2024-06-08 01:46:37 +0300

    added material UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from './page/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ frontend/src/page/Dashboard.tsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button';
+
+export default function Dashboard() {
+  return (
+    <>
+        <h1>Dashboard</h1>
+        <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
