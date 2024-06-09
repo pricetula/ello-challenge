44da75e76a8ed57b509e2e268be8a291a14d2850
Price Tula <pricetula@gmail.com>
2024-06-10 02:34:23 +0300

    clean up

--- frontend/src/page/Dashboard.tsx
+++ frontend/src/page/Dashboard.tsx
@@ -2,16 +2,12 @@ import { useQuery } from '@apollo/client';
 import { GET_BOOKS } from '../gql/queries/books';
 import BookList from '../component/BookList';
 import type { Book } from '../gql/__generated__/graphql';
-import SearchBooks from '../component/SearchBooks';
 
 export default function Dashboard() {
   const { loading, error, data } = useQuery(GET_BOOKS);
 
   if (error) return <p>Error : {error.message}</p>;
   return (
-    <>
-      <SearchBooks />
       <BookList loading={loading} books={data?.books as Book[] || []} />
-    </>
   )
 }
