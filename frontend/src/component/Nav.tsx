6b094b32c7d2cd299c724aeff2d833dbd2f463bd
Price Tula <pricetula@gmail.com>
2024-06-10 02:33:24 +0300

    add button to toggle open the search box component

--- frontend/src/component/Nav.tsx
+++ frontend/src/component/Nav.tsx
@@ -1,12 +1,14 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import logoElloSRC from '../assets/logoEllo.svg';
+import SearchBoxToggleOpenAction from './SearchBox/SearchBoxToggleOpenAction';
 
 export default function Nav() {
     return (
         <AppBar color="default" position="fixed" component="nav">
-            <Toolbar>
+            <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
                 <img src={logoElloSRC} alt="ello-logo" width={40} />
+                <SearchBoxToggleOpenAction />
             </Toolbar>
         </AppBar>
     );
