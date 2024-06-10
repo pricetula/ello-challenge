e9f5e94c384292f3e63efbdcf5ca71dfc21abf2f
Price Tula <pricetula@gmail.com>
2024-06-10 05:48:36 +0300

    updated graphql to include search query

--- frontend/src/gql/__generated__/gql.ts
+++ frontend/src/gql/__generated__/gql.ts
@@ -13,7 +13,8 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "\nquery GetBooksQuery {\n  books {\n    author\n    coverPhotoURL\n    readingLevel\n    title\n  }\n}\n": types.GetBooksQueryDocument,
+    "\n  query GetBooksQuery {\n    books {\n      author\n      coverPhotoURL\n      readingLevel\n      title\n    }\n  }\n": types.GetBooksQueryDocument,
+    "\n  query SearchBooksQuery($input: SearchTermInput!) {\n    searchBooks(input: $input) {\n      author\n      coverPhotoURL\n      readingLevel\n      title\n    }\n  }\n": types.SearchBooksQueryDocument,
 };
 
 /**
@@ -33,7 +34,11 @@ export function gql(source: string): unknown;
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(source: "\nquery GetBooksQuery {\n  books {\n    author\n    coverPhotoURL\n    readingLevel\n    title\n  }\n}\n"): (typeof documents)["\nquery GetBooksQuery {\n  books {\n    author\n    coverPhotoURL\n    readingLevel\n    title\n  }\n}\n"];
+export function gql(source: "\n  query GetBooksQuery {\n    books {\n      author\n      coverPhotoURL\n      readingLevel\n      title\n    }\n  }\n"): (typeof documents)["\n  query GetBooksQuery {\n    books {\n      author\n      coverPhotoURL\n      readingLevel\n      title\n    }\n  }\n"];
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  query SearchBooksQuery($input: SearchTermInput!) {\n    searchBooks(input: $input) {\n      author\n      coverPhotoURL\n      readingLevel\n      title\n    }\n  }\n"): (typeof documents)["\n  query SearchBooksQuery($input: SearchTermInput!) {\n    searchBooks(input: $input) {\n      author\n      coverPhotoURL\n      readingLevel\n      title\n    }\n  }\n"];
 
 export function gql(source: string) {
   return (documents as any)[source] ?? {};
--- frontend/src/gql/__generated__/graphql.ts
+++ frontend/src/gql/__generated__/graphql.ts
@@ -27,6 +27,16 @@ export type Book = {
 export type Query = {
   __typename?: 'Query';
   books?: Maybe<Array<Maybe<Book>>>;
+  searchBooks?: Maybe<Array<Maybe<Book>>>;
+};
+
+
+export type QuerySearchBooksArgs = {
+  input: SearchTermInput;
+};
+
+export type SearchTermInput = {
+  term: Scalars['String']['input'];
 };
 
 export type GetBooksQueryQueryVariables = Exact<{ [key: string]: never; }>;
@@ -34,5 +44,13 @@ export type GetBooksQueryQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type GetBooksQueryQuery = { __typename?: 'Query', books?: Array<{ __typename?: 'Book', author?: string | null, coverPhotoURL?: string | null, readingLevel?: string | null, title?: string | null } | null> | null };
 
+export type SearchBooksQueryQueryVariables = Exact<{
+  input: SearchTermInput;
+}>;
+
+
+export type SearchBooksQueryQuery = { __typename?: 'Query', searchBooks?: Array<{ __typename?: 'Book', author?: string | null, coverPhotoURL?: string | null, readingLevel?: string | null, title?: string | null } | null> | null };
+
 
 export const GetBooksQueryDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetBooksQuery"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"books"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"author"}},{"kind":"Field","name":{"kind":"Name","value":"coverPhotoURL"}},{"kind":"Field","name":{"kind":"Name","value":"readingLevel"}},{"kind":"Field","name":{"kind":"Name","value":"title"}}]}}]}}]} as unknown as DocumentNode<GetBooksQueryQuery, GetBooksQueryQueryVariables>;
+export const SearchBooksQueryDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"SearchBooksQuery"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"input"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"SearchTermInput"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"searchBooks"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"input"},"value":{"kind":"Variable","name":{"kind":"Name","value":"input"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"author"}},{"kind":"Field","name":{"kind":"Name","value":"coverPhotoURL"}},{"kind":"Field","name":{"kind":"Name","value":"readingLevel"}},{"kind":"Field","name":{"kind":"Name","value":"title"}}]}}]}}]} as unknown as DocumentNode<SearchBooksQueryQuery, SearchBooksQueryQueryVariables>;
