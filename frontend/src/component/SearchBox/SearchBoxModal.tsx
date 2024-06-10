048d47b74404fb92563df467d9166e7471f06290
Price Tula <pricetula@gmail.com>
2024-06-10 05:48:07 +0300

    added query to fetch items searched and display results on a list

--- frontend/src/component/SearchBox/SearchBoxModal.tsx
+++ frontend/src/component/SearchBox/SearchBoxModal.tsx
@@ -1,4 +1,6 @@
+import { useMemo, useState } from 'react';
 import Backdrop from '@mui/material/Backdrop';
+import { useQuery } from '@apollo/client';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
@@ -6,29 +8,54 @@ import InputAdornment from '@mui/material/InputAdornment';
 import Typography from '@mui/material/Typography';
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
+import { SEARCH_BOOKS } from '../../gql/queries/books';
 import { useAppContext } from '../../context/AppContext';
+import { useDerivedReadingBookKeys } from '../../hook/addedBookKeys';
+import { getBookKey } from '../../state/readingList';
+import SearchResultItem from './SearchResultItem';
 
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
-    transform: 'translate(-50%, -50%)',
-    maxWidth: 500,
-    minHeight: 300,
     width: "90%",
+    maxWidth: 500,
+    transform: 'translate(-50%, -50%)',
     bgcolor: 'background.paper',
     boxShadow: 24,
     p: 4,
 };
+const resultStyle = {
+    width: "100%",
+    Height: 400,
+    overflow: "auto",
+};
 
 export default function SearchBox() {
+    const [searchTerm, setSearchTerm] = useState<string>("")
     // get the appState and setAppState from AppContext
     const { setAppState, appState } = useAppContext()
+    const addedBookKeys = useDerivedReadingBookKeys()
+    const { loading, error, data } = useQuery(
+        SEARCH_BOOKS,
+        {
+            variables: { input: { term: searchTerm } },
+            skip: !searchTerm,
+        }
+    );
 
     // function to close the modal by setting openSearchBox state to false
     const handleClose = () => {
         setAppState({ openSearchBox: false })
     }
+
+    const bookResults = useMemo(() => {
+        return data?.searchBooks?.filter(
+            (book: any) => !addedBookKeys?.includes?.(getBookKey(book))
+        ) || []
+    }, [data, addedBookKeys])
+
+    if (error) return <p>Error : {error.message}</p>;
 
     return (
         <Modal
@@ -54,12 +81,20 @@ export default function SearchBox() {
                         InputProps={{
                             startAdornment: <InputAdornment position="start"><SearchIcon style={{ fontSize: 16 }} /></InputAdornment>,
                         }}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                     />
-                    <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
+                    <Box sx={resultStyle}>
+                        {
+                            (loading && <Typography>Loading...</Typography>) ||
+                            (bookResults?.length && bookResults?.length > 0 && bookResults?.map?.((book) => (
+                                book && <SearchResultItem key={getBookKey(book)} book={book}/>
+                            ))) ||
+                            (<Typography>No books found</Typography>)
+                        }
+                    </Box>
                 </Box>
             </Fade>
         </Modal>
     );
 }
+
