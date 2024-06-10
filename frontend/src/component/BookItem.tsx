59f61f8a7471a8c6563e199e4b2594201f67ca7e
Price Tula <pricetula@gmail.com>
2024-06-10 05:46:29 +0300

    added logic to remove book added to reading list store

--- frontend/src/component/BookItem.tsx
+++ frontend/src/component/BookItem.tsx
@@ -3,16 +3,38 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import ClearIcon from '@mui/icons-material/Clear';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import HeartBrokenIcon from '@mui/icons-material/HeartBroken';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
 import type { Book } from "../gql/__generated__/graphql"
+import { useReadingListStore, getBookKey } from '../state/readingList';
+import { useMemo } from 'react';
 
 export interface BookProps {
     book: Book
 }
 
 export default function BookItem({ book }: BookProps) {
+    const favouriteBookKeys = useReadingListStore((state) => state.favouriteBookKeys)
+    const removeBook = useReadingListStore((state) => state.removeBook)
+    const setFavouriteBook = useReadingListStore((state) => state.setFavouriteBook)
+    const removeFavouriteBook = useReadingListStore((state) => state.removeFavouriteBook)
+
+    const isFavouriteBook = useMemo(() => favouriteBookKeys.includes(getBookKey(book)), [favouriteBookKeys, book])
+
+    const handleRemove = (b: Book) => {
+        removeBook(getBookKey(b))
+    }
+    const handleSetOrRemoveFavourite = (b: Book) => {
+        if (isFavouriteBook) {
+            removeFavouriteBook(getBookKey(b))
+        } else {
+            setFavouriteBook(getBookKey(b))
+        }
+    }
     return (
         <Grid xs={12} md={4} lg={3}>
             <Card sx={{ maxWidth: 345 }}>
@@ -29,16 +51,26 @@ export default function BookItem({ book }: BookProps) {
                         {book.author}
                     </Typography>
                 </CardContent>
-                <CardActions>
+                <CardActions style={{ display: "flex", justifyContent: "space-between"}}>
                     <Button
                         component="label"
                         role="button"
                         variant="text"
                         size='small'
-                        startIcon={<ClearIcon />}
+                        color={isFavouriteBook ? 'error' : 'primary'}
+                        startIcon={isFavouriteBook ? <HeartBrokenIcon /> : <FavoriteIcon />}
+                        onClick={() => handleSetOrRemoveFavourite(book)}
                     >
-                        Upload file
+                        {`${isFavouriteBook ? 'remove' : 'set'} favourite`}
                     </Button>
+                    <IconButton
+                        role="button"
+                        size='small'
+                        color='error'
+                        onClick={() => handleRemove(book)}
+                    >
+                        <ClearIcon />
+                    </IconButton>
                 </CardActions>
             </Card>
         </Grid>
